7f2422da9bfd39108d3a123f8744917e045bbfb3
Zerguini_Harouit <Chemsou Harouit@10.0.7.176>
2017-10-22 12:20:30 +0100

    Creating VectorExceptionTest class

---
+++ test/tp1_igl/VectorExceptionTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1_igl;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Chemsou Harouit
+ */
+public class VectorExceptionTest {
+    
+    public VectorExceptionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of message method, of class VectorException.
+     */
+    @Test
+    public void testMessage() {
+        System.out.println("message");
+        String expResult = "";
+        String result = VectorException.message();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
